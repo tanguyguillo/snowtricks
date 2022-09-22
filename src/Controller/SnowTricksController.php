d3869331eb5fadf177ca8d362f2c1157b57d4446
Tanguy <tanguy.guillo@gmail.com>
2022-09-22 10:47:23 +0200

    globally fix .DS_Store issue

---
+++ src/Controller/SnowTricksController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SnowTricksController extends AbstractController
+{
+    #[Route('/snow/tricks', name: 'app_snow_tricks')]
+    public function index(): Response
+    {
+        return $this->render('snow_tricks/index.html.twig', [
+            'controller_name' => 'SnowTricksController',
+        ]);
+    }
+
+    /**
+     *  function home  :  http://snow-tricks.local/
+     *
+     * @return Response
+     */
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        return $this->render('snow_tricks/home.html.twig', [
+            'controller_name' => 'SnowTricksController',
+        ]);
+    }
+
+    /**
+     * function createNewTrick :  http://snow-tricks.local/index.php/create-a-new-trick
+     *
+     * @return Response
+     */
+    #[Route('/create-a-new-trick', name: 'createNewTrick')]
+    public function createNewTrick(): Response
+    {
+        return $this->render('snow_tricks/createNewTrick.html.twig', [
+            'controller_name' => 'SnowTricksController',
+        ]);
+    }
+
+    /**
+     * function modifyAtrick :  http://snow-tricks.local/index.php/modify-a-trick
+     *
+     * @return Response
+     */
+    #[Route('/modify-a-trick', name: 'modifyAtrick')]
+    public function modifyAtrick(): Response
+    {
+        return $this->render('snow_tricks/modifyAtrick.html.twig', [
+            'controller_name' => 'SnowTricksController',
+        ]);
+    }
+
+    /**
+     * function modifyAtricks   http://snow-tricks.local/index.php/presentation-of-a-trick
+     *
+     * @return Response
+     */
+    #[Route('/presentation-of-a-trick', name: 'presentationOfAtrick')]
+    public function modifyAtricks(): Response
+    {
+        return $this->render('snow_tricks/presentationOfAtrick.html.twig', [
+            'controller_name' => 'SnowTricksController',
+        ]);
+    }
+}
