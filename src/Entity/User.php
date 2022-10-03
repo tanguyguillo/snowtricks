75e4c7ad7dbe6687f60ee1a9ccca401867ff0875
Tanguy <tanguy.guillo@gmail.com>
2022-10-03 20:56:04 +0200

    make:entity User

---
+++ src/Entity/User.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $emailUser = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $username = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $passwordUser = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $roleUser = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $pictureUserUrl = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmailUser(): ?string
+    {
+        return $this->emailUser;
+    }
+
+    public function setEmailUser(?string $emailUser): self
+    {
+        $this->emailUser = $emailUser;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPasswordUser(): ?string
+    {
+        return $this->passwordUser;
+    }
+
+    public function setPasswordUser(string $passwordUser): self
+    {
+        $this->passwordUser = $passwordUser;
+
+        return $this;
+    }
+
+    public function getRoleUser(): ?string
+    {
+        return $this->roleUser;
+    }
+
+    public function setRoleUser(string $roleUser): self
+    {
+        $this->roleUser = $roleUser;
+
+        return $this;
+    }
+
+    public function getPictureUserUrl(): ?string
+    {
+        return $this->pictureUserUrl;
+    }
+
+    public function setPictureUserUrl(string $pictureUserUrl): self
+    {
+        $this->pictureUserUrl = $pictureUserUrl;
+
+        return $this;
+    }
+}
