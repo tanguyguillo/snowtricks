28b18c0c2afb5336916090a301587ad9be105cc1
Tanguy <tanguy.guillo@gmail.com>
2022-10-06 12:45:34 +0200

    persist and flush for user working

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
 use App\Entity\User;
 use App\Form\RegistrationType;
+//use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class SecurityController extends AbstractController
@@ -15,12 +18,21 @@ class SecurityController extends AbstractController
 /**
  * 
  */
-    #[Route('/registration', name: 'app_security')]
-    public function registration()
+    #[Route('/registration', name: 'app_security_registration')]
+    //public function registration(Request $request, ObjectManager  $manager)
+    public function registration(Request $request, EntityManagerInterface $manager)
     {
-        $user = new User();
+        $user = new User(); 
 
+        // binding fields
         $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($user);
+            $manager->flush();
+        }
 
         return $this->renderForm('security/registration.html.twig', [
             'form' => $form,
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationType extends AbstractType
 {
@@ -14,9 +15,9 @@ class RegistrationType extends AbstractType
         $builder
             ->add('emailUser')
             ->add('username')
-            ->add('passwordUser')
-            // ->add('roleUser')
-            // ->add('pictureUserUrl')
+            ->add('passwordUser', PasswordType::class)
+            ->add('roleUser')
+            ->add('pictureUserUrl')
         ;
     }
 
