4a5ac43ef23234734e46d7f2c9447f39a9983f3c
tanguyguillo <tanguy.guillo@gmail.com>
2022-12-23 11:50:07 +0100

    review button addNewTricks and up and down (circle))

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,12 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\SecurityBundle\Security;
 
-use  App\Controller\LoginLinkHandlerInterface;
+use App\Controller\LoginLinkHandlerInterface;
 use App\Controller\UserRepository;
 use App\Controller\Request;
 
 
 
+/**
+ * class SecurityController
+ */
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
@@ -37,32 +40,32 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
-    // /**
-    //  * function request a new link
-    //  *
-    //  * @param LoginLinkHandlerInterface $loginLinkHandler
-    //  * @param UserRepository $userRepository
-    //  * @param Request $request
-    //  * @return void
-    //  */
-    // #[Route('/request-link', name: 'request-link')]
-    // public function requestLoginLink(LoginLinkHandlerInterface $loginLinkHandler, UserRepository $userRepository, Request $request)
-    // {
-    //     // check if login form is submitted
-    //     if ($request->isMethod('POST')) {
-    //         // load the user in some way (e.g. using the form input)
-    //         $email = $request->request->get('email');
-    //         $user = $userRepository->findOneBy(['email' => $email]);
+    /**
+     * function request a new link
+     *
+     * @param LoginLinkHandlerInterface $loginLinkHandler
+     * @param UserRepository $userRepository
+     * @param Request $request
+     * @return void
+     */
+    #[Route('/request-link', name: 'request-link')]
+    public function requestLoginLink(LoginLinkHandlerInterface $loginLinkHandler, UserRepository $userRepository, Request $request)
+    {
+        // check if login form is submitted
+        if ($request->isMethod('POST')) {
+            // load the user in some way (e.g. using the form input)
+            $email = $request->request->get('email');
+            $user = $userRepository->findOneBy(['email' => $email]);
 
-    //         // create a login link for $user this returns an instance
-    //         // of LoginLinkDetails
-    //         $loginLinkDetails = $loginLinkHandler->createLoginLink($user);
-    //         $loginLink = $loginLinkDetails->getUrl();
+            // create a login link for $user this returns an instance
+            // of LoginLinkDetails
+            $loginLinkDetails = $loginLinkHandler->createLoginLink($user);
+            $loginLink = $loginLinkDetails->getUrl();
 
-    //         // ... send the link and return a response (see next section)
-    //     }
+            // ... send the link and return a response (see next section)
+        }
 
-    //     // if it's not submitted, render the "login" form
-    //     return $this->render('security/login.html.twig');
-    // }
+        // if it's not submitted, render the "login" form
+        return $this->render('security/login.html.twig');
+    }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,12 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Tricks;
+use App\Form\TricksType;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-use App\Form\TricksType;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * UserController
@@ -24,17 +24,43 @@ class UserController extends AbstractController
     }
 
 
-/**
- * addTricks
- *
- * @return void
- */
+    /**
+     * addTricks
+     * https://symfony.com/doc/current/doctrine.html#persisting-objects-to-the-database
+     * 
+     * object(App\Entity\Tricks)#759 (9) { ["id":"App\Entity\Tricks":private]=> NULL ["title":"App\Entity\Tricks":private]=> NULL ["slug":"App\Entity\Tricks":private]=> NULL ["content":"App\Entity\Tricks":private]=> NULL ["created_at":"App\Entity\Tricks":private]=> object(DateTimeImmutable)#760 (3) { ["date"]=> string(26) "2022-12-23 09:37:12.442816" ["timezone_type"]=> int(3) ["timezone"]=> string(3) "UTC" } ["active":"App\Entity\Tricks":private]=> bool(true) ["user":"App\Entity\Tricks":private]=> NULL ["category":"App\Entity\Tricks":private]=> NULL ["description":"App\Entity\Tricks":private]=> NULL }
+     *
+     * @return void
+     */
     #[Route('/tricks/details/add', name: 'app_user_tricks_add')]
-    public function addTricks()
+    public function addTricks(ManagerRegistry $doctrine): Response
     {
         $tricks =  new Tricks;
 
-        $formAddTrick = $this->createForm(TricksType::class, $tricks ); 
+        
+
+
+        $formAddTrick = $this->createForm(TricksType::class, $tricks);
+
+
+        if ($formAddTrick->isSubmitted() && $formAddTrick->isValid()) {
+            $entityManager = $doctrine->getManager();
+
+            $tricks->setSlug("test-slug");
+            
+            $entityManager->persist($tricks);
+            $entityManager->flush();
+
+            //return new Response('Saved new trick');
+            // return $this->redirectToRoute('/');
+        } else {
+            //var_dump($tricks);
+            // var_dump($formAddTrick);
+            // exit;
+            //return new Response('Not valid');
+           //return $this->redirectToRoute('/');
+        }
+
 
         return $this->render('user/tricks/add.html.twig', [
             'formAddTrick' =>  $formAddTrick->createView(),
