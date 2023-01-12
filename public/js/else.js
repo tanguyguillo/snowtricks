8c56eced57ca3de581723872ff03501b5b6bba6f
tanguyguillo <tanguy.guillo@gmail.com>
2023-01-12 19:03:39 +0100

    saving but not ok

--- public/js/else.js
+++ public/js/else.js
@@ -14,4 +14,59 @@ $(document).ready(function () {
         $('.' + id).removeClass('displayContent').addClass('displayNone');
     });
 
+
+    // // // Supression des données
+    // $("a[data-delete]").on("click", function (e) {
+    //     e.preventDefault();
+    //     var link = this;
+    //     var token = this.getAttribute("data-token");
+
+    //     // Affichage de la modale
+    //     // $("body").append(
+    //     //     '<div class="modal-confirm"><div><p>Etes-vous sur de vouloir supprimer cet article ?</p><div><button class="yes btn-validate">oui</button><button class="no btn-back">non</button></div></div></div>'
+    //     // );
+    // });
+
+    // // Confirmation ... ncaught ReferenceError: token is not defined
+    // $(".btn-validate").on("click", function () {
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "http://127.0.0.1:8000/tricks/delete-tricks/127",
+    //         data: { "_token": this.dataset.token },
+    //         success: function (response) {
+    //             console.log(response);
+    //         },
+    //         error: function (error) {
+    //             console.log(error);
+    //         },
+    //     });
+    // });
+
+    $(".btn-validate").on("click", function () {
+
+        e.preventDefault();
+        var link = this;
+        var token = this.getAttribute("data-token");
+
+        fetch(link.getAttribute("href"), {
+            method: "DELETE",
+            headers: {
+                "X-requested-with": "XMLHttpRequest",
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({ _token: this.dataset.token }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.success) {
+                    console.log(data.success);
+                    //$(".modal-confirm").remove();
+                } else {
+                    console.log(data.error);
+                }
+            })
+            .catch((e) => console.log(e));
+    });
+
+
 });
--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -18,7 +18,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;;
 
 
 /**
@@ -50,21 +50,29 @@ class TrickController extends AbstractController
      * function delete trick
      *
      */
-    #[Route('/delete-tricks/{id}', name: 'app_tricks_delete', methods: ['DELETE'])]
-    public function delete(Request $request, Tricks $trick, TricksRepository $tricksRepository)
+    #[Route('/delete-tricks/{id}', name: 'app_tricks_delete', methods: ['POST'])]
+    public function delete(Request $request, Tricks $trick, TricksRepository $tricksRepository): Response
     {
         /// we get data from json
-        $data = json_decode($request->getContent(), true);
+        //$data = json_decode($request->getContent(), true);
+
+        //dd($_POST);
+        //         array:1 [▼
+        //   "token" => "aaaad018109ff88b0e7c0920fa2f.G8yWKhdFstIFBQ2IKBLcVX-wDyVR6yqPv2axbxg8oJw.TLz7eGMWx4tTXX_tRniOMzbpO2ApuXL9iindWntS2bFapN1obXL3m090OQ"
+
 
         // we check the token
-        if ($this->isCsrfTokenValid('delete' . $trick->getId(), $data['_token'])) {
+        if (!$this->isCsrfTokenValid('delete', $request->request->get('token'))) {
+            // if ($this->isCsrfTokenValid('delete' . $trick->getId(), $data['_token'])) {
+
             dd('bien');
 
             //$tricksRepository->remove($trick, true);
 
-            return new JsonResponse(['succes' => 1]);
+            // return new JsonResponse(['succes' => 1]);
         } else {
-            return new JsonResponse(['error' => 'Token Invalide'], 400);
+            //return new JsonResponse(['error' => 'Token Invalide'], 400);
+            dd('mal');
         }
     }
 }
